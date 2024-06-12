5c814f5dc77c88a73b77064ee86c8478518b771d
Shahram Pourdashti <spourdas@yahoo.com>
2024-06-12 08:46:08 -0500

    its working now

--- parent-project/payment-service/src/main/java/com/javatechie/ps/api/PaymentServiceApplication.java
+++ parent-project/payment-service/src/main/java/com/javatechie/ps/api/PaymentServiceApplication.java
@@ -1,9 +1,11 @@
 package com.javatechie.ps.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
